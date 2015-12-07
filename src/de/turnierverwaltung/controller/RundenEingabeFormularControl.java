08edb5b3a78aa6135721ca001d925b2bf2d73af6
M. Schmuck <m_schmuck@gmx.net>
2015-12-07 13:54:11 +0100

    kleine Verbesserung

--- src/de/turnierverwaltung/controller/RundenEingabeFormularControl.java
+++ src/de/turnierverwaltung/controller/RundenEingabeFormularControl.java
@@ -19,7 +19,6 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
