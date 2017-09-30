110db938c97e978e2eb2d3ffb1931f5519202d43
Martin S. <m_schmuck@gmx.net>
2017-09-30 08:19:49 +0200

    ...

--- src/de/turnierverwaltung/view/SettingsView.java
+++ src/de/turnierverwaltung/view/SettingsView.java
@@ -179,8 +179,8 @@ public class SettingsView extends JPanel {
 		wrapper.add(htmlAll, BorderLayout.NORTH);
 		tabbedPane.addTab(Messages.getString("EigenschaftenView.5"), wrapper);
 
-//		htmlAll = new JPanel();
-//		htmlAll.setLayout(new BoxLayout(htmlAll, BoxLayout.PAGE_AXIS));
+		htmlAll = new JPanel();
+		htmlAll.setLayout(new BoxLayout(htmlAll, BoxLayout.PAGE_AXIS));
 		downloadELOListe();
 //		wrapper = new JPanel();
 //		wrapper.setLayout(new BorderLayout());
